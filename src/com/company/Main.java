1357b0bae8f004c10cd3f9d00e2f1bb955b9c61c
sophchew <98368063+sophchew@users.noreply.github.com>
2024-10-10 14:16:04 -0400

    isValidExpression done (with a bajillion edge cases), added empty string checks for helper functions.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(ShuntingYard.evaluatePostFix("5 6 * 4 /"));
+
 	// write your code here
     }
 }
--- src/com/company/ShuntingYard.java
+++ src/com/company/ShuntingYard.java
@@ -65,9 +65,81 @@ public class ShuntingYard {
     }
 
     public static boolean isValidExpression(String expressionToCheck) {
+        ArrayList<String> tokens = new ArrayList<>(Arrays.asList(expressionToCheck.split(" ")));
+        boolean foundLeft = false;
+        int lefts = 0;
+        int rights = 0;
 
+        if(!(isNumber(tokens.get(0)) || isLeftParenthesis(tokens.get(0))) || !(isNumber(tokens.get(tokens.size()-1)) || isRightParenthesis(tokens.get(tokens.size()-1)))) {
+            System.out.println("1");
+            return false;
+        }
 
-        return false;
+        if(isLeftParenthesis(tokens.get(0))) {
+            lefts++;
+            foundLeft = true;
+        }
+
+        for(int i = 1; i<tokens.size()-1; i++) {
+           if (isNumber(tokens.get(i))) {
+               if(!((isOperator(tokens.get(i-1)) || isParenthesis(tokens.get(i-1))) && (isOperator(tokens.get(i+1)) || isParenthesis(tokens.get(i+1))))) {
+                   System.out.println("2");
+                   return false;
+               }
+           } else if (isOperator(tokens.get(i))) {
+               if(!((isRightParenthesis(tokens.get(i-1)) || isNumber(tokens.get(i-1))) && (isLeftParenthesis(tokens.get(i+1)) || isNumber(tokens.get(i+1))))) {
+                   System.out.println("3");
+                   return false;
+               }
+           } else if (isLeftParenthesis(tokens.get(i))) {
+               foundLeft = true;
+               lefts++;
+               if(!((isOperator(tokens.get(i-1)) || isLeftParenthesis(tokens.get(i-1))) && (isNumber(tokens.get(i+1)) || isLeftParenthesis(tokens.get(i+1))))) {
+                   System.out.println("4");
+                   return false;
+               }
+           } else if (isRightParenthesis(tokens.get(i))) {
+               if(foundLeft) { // matching pair
+                   foundLeft = false;
+                   rights++;
+                   if(!((isNumber(tokens.get(i-1)) || isRightParenthesis(tokens.get(i-1))) && (isOperator(tokens.get(i+1)) || isRightParenthesis(tokens.get(i+1))))) {
+                       System.out.println("5");
+                       return false;
+                   }
+                   if(rights<lefts) {
+                       foundLeft = true;
+                   }
+               } else {
+                   System.out.println("6");
+                   return false;
+               }
+           } else {
+               System.out.println("7");
+               return false;
+           }
+        }
+
+        // check end token for parenthesis
+        if(isRightParenthesis(tokens.get(tokens.size()-1))) {
+            if(foundLeft) {
+                foundLeft = false;
+                rights++;
+            } else {
+                System.out.println("8");
+                return false;
+            }
+        }
+
+        if (foundLeft) { // no matching right
+            System.out.println("9");
+            return false;
+        } else if(lefts != rights) {
+            System.out.println("10");
+            return false;
+        } else {
+            return true;
+        }
+
     }
 
     public static double evaluatePostFix(String expression) {
@@ -105,16 +177,35 @@ public class ShuntingYard {
     public static boolean isNumber(String token) {
         ArrayList<String> numbers = new ArrayList<>(Arrays.asList("0", "1", "2", "3", "4", "5", "6", "7", "8", "9"));
 
+        if(token == "") {
+            return false;
+        }
+
         return numbers.contains(token.substring(0, 1));
 
     }
 
     public static boolean isLeftParenthesis(String token) {
+
+        if(token == "") {
+            return false;
+        }
+
         return token.equals("(");
     }
 
     public static boolean isRightParenthesis(String token) {
+        if(token == "") {
+            return false;
+        }
         return token.equals(")");
+    }
+
+    public static boolean isParenthesis(String token) {
+        if(token == "") {
+            return false;
+        }
+        return isLeftParenthesis(token) || isRightParenthesis(token);
     }
 
     public static boolean hasPriority(String token1, String token2) {
